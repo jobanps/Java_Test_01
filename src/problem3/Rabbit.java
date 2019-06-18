310d4dafa9ab825237fd868e47048e05ae0037ed
Joban <Joban@LAPTOP-6GDEOP3F>
2019-06-18 16:26:39 -0400

    P3 R2 Commit;

--- src/problem3/Rabbit.java
+++ src/problem3/Rabbit.java
@@ -36,5 +36,19 @@ public class Rabbit {
 	
 	// Put all your accessor methods in this section.
 	
-	
+	public int getxPosition() {
+		return xPosition;
+	}
+
+	public void setxPosition(int xPosition) {
+		this.xPosition = xPosition;
+	}
+
+	public int getyPosition() {
+		return yPosition;
+	}
+
+	public void setyPosition(int yPosition) {
+		this.yPosition = yPosition;
+	}
 }
