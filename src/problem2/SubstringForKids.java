a05114a57e8c41532d12a92405cbbb740ba6f794
Joban <Joban@LAPTOP-6GDEOP3F>
2019-06-18 16:08:46 -0400

    P2 R1

--- src/problem2/SubstringForKids.java
+++ src/problem2/SubstringForKids.java
@@ -4,10 +4,16 @@ public class SubstringForKids {
 
 	public static void main(String[] args) {
 		System.out.println("SUBSTRING FOR KIDS");
-
+		SubstringForKids obj = new SubstringForKids();
+		
+		// i = j
+		System.out.println(obj.substringForKids(2, 2, "This is a sentence"));
+		
 	}
 	
-	public String substringForKids() {
-		return "";
+	public String substringForKids(int i, int j, String sentence) {
+		
+			return "" + sentence.charAt(i);
+
 	}
 }
