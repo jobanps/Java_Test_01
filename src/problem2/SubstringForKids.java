c577922ba3b395396243b890b4abd4c356740b78
Joban <Joban@LAPTOP-6GDEOP3F>
2019-06-18 16:19:54 -0400

    P2 R4 Commit

--- src/problem2/SubstringForKids.java
+++ src/problem2/SubstringForKids.java
@@ -16,12 +16,17 @@ public class SubstringForKids {
 		//i>j
 		System.out.println(obj.substringForKids(5, 3, "bad"));
 		
+		// i is negative number
+		System.out.println(obj.substringForKids(-1, -2, "fun"));
+		
 	}
 	
 	public String substringForKids(int i, int j, String sentence) {
 		
 		String result = "";
-		if(i == j) {
+		if(i < 0 ) {
+			return "You gave me invalid numbers!";
+		} else if(i == j) {
 			return "" + sentence.charAt(i);
 		} else if(i < j){
 			
