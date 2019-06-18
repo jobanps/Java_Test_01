2058f29bc0d7627287f865d5b6472706f2f5c02c
Joban <Joban@LAPTOP-6GDEOP3F>
2019-06-18 16:13:32 -0400

    P2 R2 Commit

--- src/problem2/SubstringForKids.java
+++ src/problem2/SubstringForKids.java
@@ -7,13 +7,26 @@ public class SubstringForKids {
 		SubstringForKids obj = new SubstringForKids();
 		
 		// i = j
-		System.out.println(obj.substringForKids(2, 2, "This is a sentence"));
+		System.out.println(obj.substringForKids(1, 1, "fun"));
+		
+		//i < j
+		System.out.println(obj.substringForKids(0, 5, "peterisshouting"));
+		System.out.println(obj.substringForKids(2, 4, "apple"));
 		
 	}
 	
 	public String substringForKids(int i, int j, String sentence) {
 		
+		String result = "";
+		if(i == j)
 			return "" + sentence.charAt(i);
+		else {
+			
+			for(int a=i; a<=j ; a++) {
+				result += sentence.charAt(a);
+			}
+			return result; 
 
+		}
 	}
 }
