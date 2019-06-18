5a005d7a0c2d99ecb0410e4f49e2c4979459717b
Joban <Joban@LAPTOP-6GDEOP3F>
2019-06-18 15:57:22 -0400

    Inital Commit - Project Creation

---
+++ src/problem2/SubstringForKids.java
@@ -0,0 +1,13 @@
+package problem2;
+
+public class SubstringForKids {
+
+	public static void main(String[] args) {
+		System.out.println("SUBSTRING FOR KIDS");
+
+	}
+	
+	public String substringForKids() {
+		return "";
+	}
+}
